35b2ddab216305aeed0dbc8622498ecdbca3ec9a
JuStPiNkCoDeR <sasha.los.0148@gmail.com>
2019-11-30 00:33:44 +0300

    Version: 1.0.2 *You see nothing ;)*(#1)
    Front-end -> A little optimization.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { getCities } from './store/selectors';
+import React from 'react';
 import './scss/App.css';
 import OptionsList from './components/OptionsList';
 import CitiesList from './components/CitiesList';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <OptionsList/>
-                <CitiesList/>
-            </div>
-        )
-    }
-}
+const App = () => {
+    return(
+        <div className="App">
+            <OptionsList/>
+            <CitiesList/>
+        </div>
+    )
+};
 
-const mapStateToProps = state => ({
-    trackedCities: getCities(state)
-});
-
-export default connect(
-    mapStateToProps
-)(App);
+export default App;
